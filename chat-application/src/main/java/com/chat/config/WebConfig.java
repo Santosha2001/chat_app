aea7850b51c73e6a29f16fe6689954170901d756
Santosha C <santoshatripzzo@gmail.com>
2025-02-12 22:27:06 +0530

    chat application

--- chat-application/src/main/java/com/chat/config/WebConfig.java
+++ chat-application/src/main/java/com/chat/config/WebConfig.java
@@ -1,11 +1,26 @@
 package com.chat.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Properties;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public PropertiesPropertySource dotenvPropertySource() {
+        Dotenv dotenv = Dotenv.load(); // Load the .env file
+
+        Properties properties = new Properties();
+        dotenv.entries().forEach(entry -> properties.setProperty(entry.getKey(), entry.getValue()));
+
+        return new PropertiesPropertySource("dotenvProperties", properties);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
